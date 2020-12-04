8a121376ae7d53d4d563b2331d51499f6eeceffb
Inh3ritance <herreralaces@gmail.com>
2020-12-03 17:46:57 -0800

    Start Project

---
+++ MultiClient.java
@@ -0,0 +1,15 @@
+import java.security.Provider;
+
+public class MultiClient{
+  public static void main(String[] args){
+    final String secretKey = "ssshhhhhhhhhhh!!!!";
+     
+    String originalString = "howtodoinjava.com";
+    String encryptedString = AES.encrypt(originalString, secretKey) ;
+    String decryptedString = AES.decrypt(encryptedString, secretKey) ;
+     
+    System.out.println(originalString);
+    System.out.println(encryptedString);
+    System.out.println(decryptedString);
+  }
+}
